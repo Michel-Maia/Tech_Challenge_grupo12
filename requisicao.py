f517a91a05c5941c66012bc7798768093deb4ada
Lucas Souza <lucas.souza.andrade64@gmail.com>
2024-05-07 18:32:54 -0300

    Update requisicao.py

--- requisicao.py
+++ requisicao.py
@@ -1,20 +1,25 @@
 import csv
 import requests
+from fastapi import FastAPI
 
-resposta = {}
-urlBase = "http://vitibrasil.cnpuv.embrapa.br/download/"
-arquivos = ["Producao", "ProcessaViniferas", "ProcessaAmericanas", "ProcessaMesa", "ProcessaSemclass", "Comercio", "ImpVinhos", 
-            "ImpEspumantes", "ImpFrescas", "ImpPassas", "ImpSuco", "ExpVinho", "ExpEspumantes", "ExpUva", "ExpSuco"]
+# uvicorn main:app --reload
+app = FastAPI()
 
-for arquivo in arquivos:
-    url = f"{urlBase}{arquivo}.csv"
-    response = requests.get(url)
-    if response.status_code != 200:
-        resposta = {'erro': response.status_code}
-        break
-    reader = csv.DictReader(response.text.splitlines())
-    resposta[arquivo] = []
-    for linha in reader:
-        resposta[arquivo].append(linha)
+@app.get('/')
+def resquest_embrapa():
+    resposta = {}
+    urlBase = 'http://vitibrasil.cnpuv.embrapa.br/download/'
+    arquivos = ['Producao', 'ProcessaViniferas', 'ProcessaAmericanas', 'ProcessaMesa', 'ProcessaSemclass', 'Comercio', 'ImpVinhos', 
+                'ImpEspumantes', 'ImpFrescas', 'ImpPassas', 'ImpSuco', 'ExpVinho', 'ExpEspumantes', 'ExpUva', 'ExpSuco']
 
-print(resposta)
+    for arquivo in arquivos:
+        url = f'{urlBase}{arquivo}.csv'
+        response = requests.get(url)
+        if response.status_code != 200:
+            resposta = {'erro': response.status_code}
+            break
+        reader = csv.DictReader(response.text.splitlines())
+        resposta[arquivo] = []
+        for linha in reader:
+            resposta[arquivo].append(linha)
+    return resposta
