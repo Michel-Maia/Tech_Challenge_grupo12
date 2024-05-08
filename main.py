6d1119b9387c85f2a705ed53776c0a751729307a
Lucas Souza <lucas.souza.andrade64@gmail.com>
2024-05-08 12:25:03 -0300

    Código refatorado
    Código refatorado para separar o FastAPI da lógica de requisição ao Embrapa.

--- main.py
+++ main.py
@@ -2,44 +2,43 @@ import csv
 import requests
 from fastapi import FastAPI
 
-app = FastAPI()
-
-@app.get('/')
-def request_embrapa_geral():
-    resposta = {}
+def request_embrapa(arquivo = None):
     urlBase = 'http://vitibrasil.cnpuv.embrapa.br/download/'
     arquivos = ['Producao', 'ProcessaViniferas', 'ProcessaAmericanas', 'ProcessaMesa', 'ProcessaSemclass', 'Comercio', 'ImpVinhos', 
-                'ImpEspumantes', 'ImpFrescas', 'ImpPassas', 'ImpSuco', 'ExpVinho', 'ExpEspumantes', 'ExpUva', 'ExpSuco']
-
-    for arquivo in arquivos:
-        url = f'{urlBase}{arquivo}.csv'
-        response = requests.get(url)
-        if response.status_code != 200:
-            resposta = {'erro': response.status_code}
-            break
-        reader = csv.DictReader(response.text.splitlines())
-        resposta[arquivo] = []
-        for linha in reader:
-            resposta[arquivo].append(linha)
-    return resposta
-
-@app.get('/{arquivo:str}')
-def request_embrapa_especifico(arquivo: str):
+                'ImpEspumantes', 'ImpFrescas', 'ImpPassas', 'ImpSuco', 'ExpVinho', 'ExpEspumantes', 'ExpUva', 'ExpSuco']   
     resposta = {}
-    urlBase = 'http://vitibrasil.cnpuv.embrapa.br/download/'
-    arquivos = ['Producao', 'ProcessaViniferas', 'ProcessaAmericanas', 'ProcessaMesa', 'ProcessaSemclass', 'Comercio', 'ImpVinhos', 
-                'ImpEspumantes', 'ImpFrescas', 'ImpPassas', 'ImpSuco', 'ExpVinho', 'ExpEspumantes', 'ExpUva', 'ExpSuco']
-
-    if arquivo in arquivos:
-        url = f'{urlBase}{arquivo}.csv'
-        response = requests.get(url)
-        if response.status_code != 200:
-            resposta = {'erro': response.status_code}
-        else:
+    if arquivo == None:
+        for arquivo in arquivos:
+            url = f'{urlBase}{arquivo}.csv'
+            response = requests.get(url)
+            if response.status_code != 200:
+                resposta = {'erro': response.status_code}
+                break
             reader = csv.DictReader(response.text.splitlines())
             resposta[arquivo] = []
             for linha in reader:
                 resposta[arquivo].append(linha)
+        return resposta
+    elif arquivo in arquivos:
+            url = f'{urlBase}{arquivo}.csv'
+            response = requests.get(url)
+            if response.status_code != 200:
+                resposta = {'erro': response.status_code}
+            else:
+                reader = csv.DictReader(response.text.splitlines())
+                resposta[arquivo] = []
+                for linha in reader:
+                    resposta[arquivo].append(linha)
     else:
-        resposta = {'erro': 404}
+        resposta = {'erro': 404} 
     return resposta
+
+app = FastAPI()
+
+@app.get('/')
+def get_todos_arquivos():
+    return request_embrapa()
+
+@app.get('/{arquivo:str}')
+def get_arquivo(arquivo:str):
+    return request_embrapa(arquivo)
