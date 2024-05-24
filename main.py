c8d9c1931c05cdab8b31677b961461c11779d943
Lucas Souza <lucas.souza.andrade64@gmail.com>
2024-05-24 14:16:57 -0300

    Adição do Streamlit

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import utils.embrapa
 from fastapi import FastAPI
 
-app = FastAPI()
+api = FastAPI()
 
-@app.get('/dados')
+@api.get('/dados')
 def dados_arquivos():
     return utils.embrapa.request()
 
-@app.get('/dados/{arquivo:str}')
+@api.get('/dados/{arquivo:str}')
 def dados_arquivo(arquivo:str):
     return utils.embrapa.request(arquivo)
 
-@app.get('/listar_arquivos')
+@api.get('/listar_arquivos')
 def listar_arquivos():
     return {'arquivos': utils.embrapa.arquivos}
