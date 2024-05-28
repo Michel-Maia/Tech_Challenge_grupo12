523a1a5153cccbb70f82d030815ebbd7d3c3a67f
Lucas Souza <lucas.souza.andrade64@gmail.com>
2024-05-28 17:35:27 -0300

    Reformulando index.html

--- app.py
+++ app.py
@@ -3,28 +3,35 @@ import requests
 import json
 
 API_URL = 'http://localhost:8000'
+
+st.title('Frontend de Teste para API Embrapa')
+
 endpoints = {
-    '/dados': 'Obter os dados de todos arquivos',
-    '/dados/{arquivo}': 'Obter os dados de um arquivo específico',
+    '/dados': 'Obter todos os dados',
+    '/dados/{arquivo}': 'Obter dados de um arquivo específico',
     '/listar_arquivos': 'Listar arquivos disponíveis'
 }
 
-st.title('Frontend de teste para API Embrapa')
-endpointSelecionado = st.selectbox('Selecione um endpoint:', list(endpoints.keys()))
-st.write(endpoints[endpointSelecionado])
+selected_endpoint = st.selectbox('Selecione um endpoint:', list(endpoints.keys()))
+st.write(endpoints[selected_endpoint])
 
-if endpointSelecionado == '/dados/{arquivo}':
+if selected_endpoint == '/dados/{arquivo}':
     arquivo = st.text_input('Nome do arquivo:')
 else:
     arquivo = None
 
-if st.button('Fazer requisição'):
-    url = f'{API_URL}{endpointSelecionado}'
+if st.button('Fazer Requisição'):
+    url = f'{API_URL}{selected_endpoint}'
     if arquivo:
         url = url.format(arquivo=arquivo)
+
     response = requests.get(url)
+
     if response.status_code == 200:
-        conteudoCSV = response.json()
-        st.json(conteudoCSV)
+        try:
+            data = response.json()
+            st.json(data)
+        except json.JSONDecodeError:
+            st.text(response.text)
     else:
         st.error(f'Erro na requisição: {response.status_code}')
